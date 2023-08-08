c67799ed8c9b3aab3318a7b60b9f5d1c47d0837b
GabrielVRV <62068155+GabrielVRV@users.noreply.github.com>
2023-08-08 12:56:32 -0300

    Added new files
    Added C# software for encoding and decoding Base64 files and multiplication of numbers with more than 22 digits

---
+++ Multiplicação BigInt/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Numerics;
+
+namespace  BigIntMultiplica
+{
+    class Program{
+        static void Main(string[] args){
+
+            bool repeatAux = true;
+
+            //Criando uma estrutura de repetição para caso seja necessário realizar mais de uma operação
+            while (repeatAux){    
+                Console.WriteLine("Digite o primeiro número:");
+                string inputNum1 = Console.ReadLine();
+
+                Console.WriteLine("Digite o segundo número:");
+                string inputNum2 = Console.ReadLine();
+
+                //Realizando a conversão de String para BigInteger e ao mesmo tempo verifica se é um número
+                if (BigInteger.TryParse(inputNum1, out BigInteger num1) &&
+                    BigInteger.TryParse(inputNum2, out BigInteger num2))
+                {
+                    //Caso tenha conseguido realizar a conversão, realiza a multiplicação e exibe o resultado
+                    BigInteger result = num1 * num2;
+                    Console.WriteLine("O resultado da multiplicação é:");
+                    Console.WriteLine(result);
+
+                    //Verifica se é necessário realizar uma nova operação
+                    Console.WriteLine("Você deseja realizar uma nova operação? Digite S para sim e N para não");
+                    string aux = Console.ReadLine();
+
+                    /*Caso a resposta seja sim ele mantem a variavel repeatAux como verdadeira fazendo
+                      com que repita todo o processo*/
+                    if(aux == "s" || aux == "S"){
+                        repeatAux = true;
+                    }
+
+                    /*Caso a resposta seja diferente de sim ele altera a variavel para false fazendo com
+                      que saia do loop e assim encerre o programa */
+                    else{
+                        repeatAux = false;
+                    }
+                    
+                }
+                else
+                {
+                    /*Caso seja verificado que não possível converter para BigInt significa que não é um número
+                    então solicita que seja inserido um número válido*/
+                    Console.WriteLine("Entrada inválida! Certifique-se de digitar números válidos.");
+                    repeatAux = true;
+                }
+            }       
+        }
+    }
+    
+}
---
+++ base64Cod-Decod/base64/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.IO;
+
+namespace Base64EncoderDecoder
+{
+    class Program
+    {
+        //Criando o processo de codificação
+        static void EncodeFile(string inputPath, string outputPath)
+        {
+            byte[] data = File.ReadAllBytes(inputPath);
+            string base64String = Convert.ToBase64String(data);
+            File.WriteAllText(outputPath, base64String);
+        }
+        
+        //Criando o processo de decodificação
+        static void DecodeFile(string inputPath, string outputPath)
+        {
+            string base64String = File.ReadAllText(inputPath);
+            byte[] data = Convert.FromBase64String(base64String);
+            File.WriteAllBytes(outputPath, data);
+        }
+
+        //Criando oque vai aparecer para quem utilizar o software
+        static void Main(string[] args)
+        {
+            Console.WriteLine("1 - Codificar arquivo para Base64");
+            Console.WriteLine("2 - Decodificar arquivo de Base64");
+            Console.Write("Escolha uma opção (1/2): ");
+            string choice = Console.ReadLine();
+
+            if (choice == "1")
+            {
+                Console.Write("Digite o caminho do arquivo de entrada: ");
+                string inputPath = Console.ReadLine();
+                Console.Write("Digite o caminho do arquivo de saída codificado: ");
+                string outputPath = Console.ReadLine();
+                EncodeFile(inputPath, outputPath);
+                Console.WriteLine("Arquivo codificado com sucesso!");
+            }
+            else if (choice == "2")
+            {
+                Console.Write("Digite o caminho do arquivo de entrada codificado: ");
+                string inputPath = Console.ReadLine();
+                Console.Write("Digite o caminho do arquivo de saída decodificado: ");
+                string outputPath = Console.ReadLine();
+                DecodeFile(inputPath, outputPath);
+                Console.WriteLine("Arquivo decodificado com sucesso!");
+            }
+            else
+            {
+                Console.WriteLine("Opção inválida. Escolha 1 ou 2.");
+            }
+        }
+    }
+}
